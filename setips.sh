0dd7b73f9f4953d82c6ad0312d8c60bfe1cf8c94
spatiald <spatiald@users.noreply.github.com>
2021-04-12 23:52:25 -0400

    Update setips.sh

--- setips.sh
+++ setips.sh
@@ -8,7 +8,7 @@
 # Author : spatiald
 ############################################################################
 
-scriptVersion=3.1c
+scriptVersion=3.1d
 
 # Check that we're root
 if [[ $UID -ne 0 ]]; then
